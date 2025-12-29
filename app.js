0990db443e231480303f949f3a62405db457e849
Harsh Foujdar <harshbtp738@gmail.com>
2025-12-29 22:57:02 +0530

    update

--- app.js
+++ app.js
@@ -3,37 +3,45 @@ const path = require('path');
 
 // External Module
 const express = require('express');
+const mongoose = require('mongoose');
 
-//Local Module
-const storeRouter = require("./routes/storeRouter")
-const hostRouter = require("./routes/hostRouter")
+// Local Module
+const storeRouter = require("./routes/storeRouter");
+const hostRouter = require("./routes/hostRouter");
 const rootDir = require("./utils/pathUtil");
 const errorsController = require("./controllers/errors");
-const { mongoConnect } = require('./utils/databaseutil');
-const { default: mongoose } = require('mongoose');
 
 const app = express();
 
+// View Engine
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(express.urlencoded());
+// Body Parser
+app.use(express.urlencoded({ extended: false }));
+
+//  STATIC FILES (IMPORTANT: routes se pehle)
+app.use(express.static(path.join(rootDir, 'public')));
+
+// Routes
 app.use(storeRouter);
 app.use("/host", hostRouter);
 
-app.use(express.static(path.join(rootDir, 'public')))
-
+// 404 Page
 app.use(errorsController.pageNotFound);
 
+// Environment variables
 const PORT = process.env.PORT || 3000;
-const MOUNGO_URL = "mongodb+srv://airbnbUser:Harsh123@airbnb.xxylmkm.mongodb.net/airbnb?retryWrites=true&w=majority";
-mongoose.connect(MOUNGO_URL).then(() => {
-  console.log("Database connected");
+const MONGO_URL = process.env.MONGO_URL;
 
-  app.listen(PORT, () => {
-    console.log(`Server running on address http://localhost:${PORT}`);
+// Database + Server
+mongoose.connect(MONGO_URL)
+  .then(() => {
+    console.log(" Database connected");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log(" Database connection error:", err);
   });
-}).catch((err) => {
-  console.log("Error was  started", err);
-
-})
